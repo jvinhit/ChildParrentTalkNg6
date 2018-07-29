5704230b543fb489346331ff3ff8af46256d18bb
Vĩnh Nguyễn <jvinhit@gmail.com>
2018-07-29 15:55:43 +0700

    Up to date alias

--- src/app/switches/switches.component.ts
+++ src/app/switches/switches.component.ts
@@ -7,15 +7,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
     @Input() checked: boolean;
-    @Output() change = new EventEmitter<boolean>();
+    // tslint:disable-next-line:no-output-rename
+    @Output('checkedChanged') change = new EventEmitter<boolean>();
     constructor() {}
 
     ngOnInit() {
         console.log(` __: ${this.checked}`);
     }
     emitChangeValue(event: any) {
-        event.stopPropagation();
-        console.log(event.target.checked);
+        // event.stopPropagation();
+        // console.log(event.target.checked);
         this.change.emit(event.target.checked);
     }
 }
